b6d6c2c5a41f755328a06b18daf22450e71b3f27
kevin <kevin@DESKTOP-MNSRO8D>
2021-04-07 21:39:32 +0800

    add comment of label

--- sniff.py
+++ sniff.py
@@ -12,6 +12,9 @@ class Sniffer:
             self.fields_ = json.load(f)
 
     def Sniff(self, count = 200, iface = conf.iface):
+        '''
+        count : count of packet to capture 
+        '''
         self.packets_ = sniff(iface = iface, count = count)
         return self.packets_
 
@@ -34,31 +37,40 @@ class Sniffer:
         command += " -E header=y -E separator=, -E quote=d -E occurrence=f > {}".format(csvName)
         os.system(command)
 
-    # def CheckField(self):
-    #     if self.packets_ is None:
-    #         raise Exception("No packets!")
+    def CheckField(self):
+        '''
+        Never use this function
+        '''
+        if self.packets_ is None:
+            raise Exception("No packets!")
 
-    #     all_layers = []
-    #     for packet in self.packets_:
-    #         for layer in packet.layers():
-    #             if layer not in all_layers:
-    #                 all_layers.append(layer)
+        all_layers = []
+        for packet in self.packets_:
+            for layer in packet.layers():
+                if layer not in all_layers:
+                    all_layers.append(layer)
         
-    #     all_fields = list(set([layer.__name__.lower() for layer in all_layers]))
-    #     resolved = [layer_ for layer_ in all_fields if layer_ in self.fields_]
-    #     unresolved = [layer_ for layer_ in all_fields if layer_ not in self.fields_]
+        all_fields = list(set([layer.__name__.lower() for layer in all_layers]))
+        resolved = [layer_ for layer_ in all_fields if layer_ in self.fields_]
+        unresolved = [layer_ for layer_ in all_fields if layer_ not in self.fields_]
 
-    #     print("unresolved:", unresolved)
+        print("unresolved:", unresolved)
 
 if __name__ == '__main__':
     sniffer = Sniffer()
+
+    # begin capture packets and set count of packets to 10000  
+    # sniffer.Sniff(10000)
+    
+    # write captured packets to pcapfile.pcap
+    # sniffer.W2pcap("pcapfile.pcap")
 
     # convert test.pcap to test.csv
     sniffer.pcap2csv("test.pcap", "test.csv")
 
 
 
-
+# just show an example of using tshark
 '''
 tshark -r demo.pcap -T fields -e eth.src -e eth.dst -e ip.src -e ip.dst -e ip.proto -E header=y -E separator=, -E quote=d -E occurrence=f > test.csv
 '''
